7d59c788e845bdeb1d73fef62f9313a320823f72
lars-hagen <larsh2690@hotmail.com>
2026-08-05 21:59:11 +0200

    feat: add --version and report versions on upgrade

--- nookwire_ssh.py
+++ nookwire_ssh.py
@@ -25,7 +25,7 @@ from pathlib import Path
 import asyncssh
 
 
-VERSION = "1.4.0"
+VERSION = "1.4.1"
 DEFAULT_PASSWORD_ENV = "NOOKWIRE_SSH_PASSWORD"
 DEFAULT_HOST_KEY = (
     Path(tempfile.gettempdir()) / f"nookwire-ssh-{os.geteuid()}" / "host-key"
