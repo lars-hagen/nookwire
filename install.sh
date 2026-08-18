d22eac486345cf4371119262c017d502fc68bea9
lars-hagen <larsh2690@hotmail.com>
2026-08-18 05:13:15 +0200

    chore: bump version to 2.2.0

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ set -eu
 #   NOOKWIRE_SSH_PREFIX    install the console script into $PREFIX/bin.
 #   UV_TOOL_DIR / UV_TOOL_BIN_DIR  standard uv tool isolation (honored).
 
-VERSION=${NOOKWIRE_SSH_VERSION:-2.1.3}
+VERSION=${NOOKWIRE_SSH_VERSION:-2.2.0}
 PACKAGE=${NOOKWIRE_SSH_PACKAGE:-}
 if [ -z "$PACKAGE" ]; then
   BASE=${NOOKWIRE_SSH_BASE_URL:-git+https://github.com/lars-hagen/nookwire-ssh}
--- src/nookwire_ssh/__init__.py
+++ src/nookwire_ssh/__init__.py
@@ -1,3 +1,3 @@
 """Nookwire SSH: temporary SSH, SFTP, and SCP access to ephemeral workspaces."""
 
-__version__ = "2.1.3"
+__version__ = "2.2.0"
